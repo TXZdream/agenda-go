33a9cd24ad49544faf1f2d9d6f2b465e4c78ae3a
freakkid <904811062@qq.com>
2017-10-28 17:20:21 +0800

    modify test again

--- test/t.go
+++ test/t.go
@@ -1,6 +1,7 @@
 package main
 import (
 	"github.com/txzdream/agenda-go/entity/service"
+	"github.com/txzdream/agenda-go/entity/model"
 	"fmt"
 )
 
@@ -9,7 +10,9 @@ func main() {
 	service.StartAgenda(&k)
 	fmt.Println(k)
 	fmt.Println(k.AgendaStorage)
-	k.AgendaStorage.CurrentUser = service.User{"sads", "SDfd", "asds", "sadsd"}
+	aa := []model.User{}
+	aa = append(aa, model.User{"sads", "SDfd", "asds", "sadsd"})
+	k.AgendaStorage.Users = aa
 	fmt.Println(k.AgendaStorage)
 
 	var m service.Service
@@ -18,13 +21,13 @@ func main() {
 	fmt.Println(m.AgendaStorage)
 	// fmt.Println(k.CurrentUser.UserName == "")
 	//  ------------------------------------
-	a := []int{1, 2, 3, 4, 5, 6, 7, 8,9}
-	for index, t := range a {
-		if t == 9 {
-			a = append(a[:index], a[index+1:]...)
-			break
-		}
-	}
-	fmt.Println(a)
+	// a := []int{1, 2, 3, 4, 5, 6, 7, 8,9}
+	// for index, t := range a {
+	// 	if t == 9 {
+	// 		a = append(a[:index], a[index+1:]...)
+	// 		break
+	// 	}
+	// }
+	// fmt.Println(a)
 }
 // 开启读取数据--》获取当前用户名--》是否登陆--》再获取之后的命令--》进行之后的操作--》退出并写入文件
